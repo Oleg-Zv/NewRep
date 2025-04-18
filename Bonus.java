e419e5e41628cca00a0578d4fafa09b4b28e72ff
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-04-18 09:46:55 +0500

    manual project

---
+++ Bonus.java
@@ -0,0 +1,23 @@
+public class Bonus {
+    public int x;
+    public int y;
+    public int points;
+
+    public Bonus(int x, int y, int points) {
+        this.x = x;
+        this.y = y;
+        this.points = points;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
